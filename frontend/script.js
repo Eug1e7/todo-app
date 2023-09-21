38eea361238011f9f259b3e42dd331c1bc4fd8b4
中村 香貴 <nakamura-ko@lakeel.com>
2023-09-21 17:44:15 +0900

    frontendのリファクタリング

--- frontend/script.js
+++ frontend/script.js
@@ -1,44 +1,47 @@
-// script.js
-
+// サーバーから全てのタスクを取得する関数
 async function fetchTasks() {
-  const response = await fetch('http://localhost:3000/api/todos');
+  const response = await fetch("http://localhost:3000/api/todos");
   const data = await response.json();
   return data;
 }
 
+// 新しいタスクをサーバーに追加する関数
 async function addTask(task) {
-  const response = await fetch('http://localhost:3000/api/todos', {
-    method: 'POST',
+  const response = await fetch("http://localhost:3000/api/todos", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ task })
+    body: JSON.stringify({ task }),
   });
   const data = await response.json();
   return data;
 }
 
+// タスクを画面に表示する関数
 async function renderTasks() {
   const tasks = await fetchTasks();
-  const list = document.getElementById('todoList');
-  list.innerHTML = '';
-  for (let i = 0; i < tasks.length; i++) {
-    const item = document.createElement('div');
-    item.className = 'todoItem';
-    item.innerText = tasks[i];
+  const list = document.getElementById("todoList");
+  list.innerHTML = "";
+  for (const task of tasks) {
+    const item = document.createElement("div");
+    item.className = "todoItem";
+    item.innerText = task;
     list.appendChild(item);
   }
+
 }
 
+// タスク追加ボタンがクリックされたときの処理
 async function handleAddTask() {
-  const newTaskInput = document.getElementById('newTask');
+  const newTaskInput = document.getElementById("newTask");
   const newTask = newTaskInput.value;
   if (newTask) {
     await addTask(newTask);
-    newTaskInput.value = '';
+    newTaskInput.value = "";
     await renderTasks();
   }
 }
 
-// Initial rendering
+// 初期表示時にタスクを画面に表示
 renderTasks();
