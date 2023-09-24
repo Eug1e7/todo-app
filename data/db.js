8ae0b2471eb92b8b949685f2062bbe285da3de1a
Euglena <shimidorimu.00@gmail.com>
2023-09-24 17:14:03 +0900

    ポートを6160に変更

--- data/db.js
+++ data/db.js
@@ -1,11 +1,11 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'mytododb',
-  password: '2525',
-  port: 5432,
+  user: "postgres",
+  host: "localhost",
+  database: "mytododb",
+  password: "2525",
+  port: 6160,
 });
 
 module.exports = pool;
